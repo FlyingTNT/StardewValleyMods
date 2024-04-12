3c78ae892b45ff30826ad4277bb16c5322ba6ac7
C. Cooper <c.cooper@ku.edu>
2024-04-12 15:59:19 -0500

    Add CatalogueFilter

---
+++ CatalogueFilter/ModConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using System.Collections.Generic;
+
+namespace CatalogueFilter
+{
+    public class ModConfig
+    {
+        public bool ModEnabled { get; set; } = true;
+        public bool ShowLabel { get; set; } = true;
+        public Color LabelColor { get; set; } = Color.White; // Color for the text that says "Filter" in the shop menu
+    }
+}

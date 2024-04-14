938c86ce056ab9814f44b4fffe3a82615cf2ef72
C. Cooper <c.cooper@ku.edu>
2024-04-14 17:19:42 -0500

    Add ResourceStorage

---
+++ ResourceStorage/CodePatches.cs
@@ -0,0 +1,350 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using StardewValley;
+using StardewValley.GameData.Objects;
+using StardewValley.Inventories;
+using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using Object = StardewValley.Object;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+
+namespace ResourceStorage
+{
+    public partial class ModEntry
+    {
+        [HarmonyPatch(typeof(Inventory), nameof(Inventory.ReduceId))]
+        public class Inventory_ReduceId_Patch
+        {
+            public static bool Prefix(Inventory __instance, string itemId, ref int count)
+            {
+                if (!Config.ModEnabled || !Config.AutoUse)
+                    return true;
+
+                if(!TryGetInventoryOwner(__instance, out Farmer farmer))
+                {
+                    return true;
+                }
+
+                count += (int)ModifyResourceLevel(farmer, ItemRegistry.QualifyItemId(itemId), -count);
+                return count > 0;
+            }
+        }
+
+        [HarmonyPatch(typeof(Farmer), nameof(Farmer.addItemToInventory), new Type[] { typeof(Item), typeof(List<Item>) })]
+        public class Farmer_addItemToInventory_Patch
+        {
+            public static bool Prefix(Farmer __instance, Item item)
+            {
+                if (!Config.ModEnabled || Game1.activeClickableMenu is ResourceMenu || item is not Object || !CanStore(item as Object))
+                    return true;
+                return ModifyResourceLevel(__instance, item.QualifiedItemId, item.Stack) <= 0;
+            }
+        }
+
+        [HarmonyPatch(typeof(Inventory), nameof(Inventory.CountId))]
+        public class Inventory_CountId_Patch
+        {
+            public static void Postfix(Inventory __instance, string itemId, ref int __result)
+            {
+                if (!Config.ModEnabled || !Config.AutoUse || !ModEntry.TryGetInventoryOwner(__instance, out Farmer farmer))
+                    return;
+
+                __result += (int)GetResourceAmount(farmer, ItemRegistry.QualifyItemId(itemId));
+            }
+        }
+
+        [HarmonyPatch(typeof(Farmer), nameof(Farmer.getItemCount))]
+        public class Farmer_getItemCount_Patch
+        {
+            public static void Postfix(Farmer __instance, string itemId, ref int __result)
+            {
+                if (!Config.ModEnabled || !Config.AutoUse)
+                    return;
+
+                __result += (int)GetResourceAmount(__instance, ItemRegistry.QualifyItemId(itemId));
+            }
+        }
+
+        [HarmonyPatch(typeof(Inventory), nameof(Inventory.ContainsId), new Type[] {typeof(string)})]
+        public class Inventory_ContainsId_Patch
+        {
+            public static void Postfix(Inventory __instance, string itemId, ref bool __result)
+            {
+                if (__result || !Config.ModEnabled || !Config.AutoUse || !TryGetInventoryOwner(__instance, out Farmer farmer))
+                    return;
+
+                __result = (int)GetResourceAmount(farmer, ItemRegistry.QualifyItemId(itemId)) > 0;
+            }
+        }
+
+        [HarmonyPatch(typeof(Inventory), nameof(Inventory.ContainsId), new Type[] { typeof(string), typeof(int) })]
+        public class Inventory_ContainsId2_Patch
+        {
+            public static void Postfix(Inventory __instance, string itemId, int minimum, ref bool __result)
+            {
+                if (__result || !Config.ModEnabled || !Config.AutoUse || !TryGetInventoryOwner(__instance, out Farmer farmer))
+                    return;
+
+                __result = (int)GetResourceAmount(farmer, ItemRegistry.QualifyItemId(itemId)) >= minimum;
+            }
+        }
+
+        [HarmonyPatch(typeof(Farmer), nameof(Farmer.couldInventoryAcceptThisItem), new Type[] {typeof(Item)})]
+        public class Farmer_couldInventoryAcceptThisItem_Patch
+        {
+            public static void Postfix(Farmer __instance, Item item, ref bool __result)
+            {
+                if (!Config.ModEnabled || __result || item is not Object || !CanStore(item as Object))
+                    return;
+
+                if (GetResourceAmount(__instance, item.QualifiedItemId) > 0 || CanAutoStore(item.QualifiedItemId))
+                    __result = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Farmer), nameof(Farmer.couldInventoryAcceptThisItem), new Type[] { typeof(string), typeof(int), typeof(int) })]
+        public class Farmer_couldInventoryAcceptThisItem2_Patch
+        {
+            public static void Postfix(Farmer __instance, string id, int quality, ref bool __result)
+            {
+                if (!Config.ModEnabled || __result || quality > 0)
+                    return;
+
+                string qualifiedId = ItemRegistry.QualifyItemId(id);
+                if (GetResourceAmount(__instance, qualifiedId) > 0 || CanAutoStore(qualifiedId))
+                    __result = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Object), nameof(Object.ConsumeInventoryItem), new Type[] { typeof(Farmer), typeof(Item), typeof(int) })]
+        public class Object_ConsumeInventoryItem_Patch
+        {
+            public static bool Prefix(Farmer who, Item drop_in, ref int amount)
+            {
+                if (!Config.ModEnabled || !Config.AutoUse)
+                    return true;
+
+                amount += (int)ModifyResourceLevel(who, drop_in.QualifiedItemId, -amount);
+                return amount > 0;
+            }
+        }
+
+        [HarmonyPatch(typeof(CraftingRecipe), nameof(CraftingRecipe.ConsumeAdditionalIngredients))]
+        public class CraftingRecipe_ConsumeAdditionalIngredients_Patch
+        {
+            public static void Prefix(List<KeyValuePair<string, int>> additionalRecipeItems)
+            {
+                if (!Config.ModEnabled || !Config.AutoUse)
+                    return;
+                for(int i = 0; i < additionalRecipeItems.Count; i++)
+                {
+                    additionalRecipeItems[i] = new KeyValuePair<string, int>(additionalRecipeItems[i].Key, additionalRecipeItems[i].Value + (int)ModifyResourceLevel(Game1.player, ItemRegistry.QualifyItemId(additionalRecipeItems[i].Key), -additionalRecipeItems[i].Value));
+                }
+            }
+        }
+        [HarmonyPatch(typeof(CraftingRecipe), nameof(CraftingRecipe.getCraftableCount), new Type[] { typeof(IList<Item>) })]
+        public class CraftingRecipe_getCraftableCount_Patch
+        {
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                SMonitor.Log($"Transpiling CraftingRecipe.getCraftableCount");
+                var codes = new List<CodeInstruction>(instructions);
+                for (int i = 0; i < codes.Count; i++)//Broken
+                {
+                    if (codes[i].opcode == OpCodes.Ldloc_3 && codes[i + 1].opcode == OpCodes.Ldloc_S && codes[i + 2].opcode == OpCodes.Div)
+                    {
+                        SMonitor.Log($"adding method to increase ingredient count");
+                        codes.Insert(i + 1, new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ModEntry), nameof(AddIngredientAmount))));
+                        codes.Insert(i + 1, new CodeInstruction(OpCodes.Ldloc_2));
+                    }
+                }
+
+                return codes.AsEnumerable();
+            }
+        }
+
+        [HarmonyPatch(typeof(CraftingRecipe), nameof(CraftingRecipe.consumeIngredients))]
+        public class CraftingRecipe_consumeIngredients_Patch
+        {
+            public static void Prefix(CraftingRecipe __instance, ref Dictionary<string, int> __state)
+            {
+                if (!Config.ModEnabled || !Config.AutoUse)
+                    return;
+
+                __state = __instance.recipeList;
+                Dictionary<string, int> dict = new();
+                foreach(var s in __state)
+                {
+                    if (!Game1.objectData.ContainsKey(s.Key))
+                    {
+                        dict.Add(s.Key, s.Value);
+                        continue;
+                    }
+                    int amount = s.Value + (int)ModifyResourceLevel(Game1.player, ItemRegistry.QualifyItemId(s.Key), -s.Value);
+                    if (amount <= 0)
+                        continue;
+                    dict.Add(s.Key, amount);
+                }
+                __instance.recipeList = dict;
+            }
+            public static void Postfix(CraftingRecipe __instance, ref Dictionary<string, int> __state)
+            {
+                if (!Config.ModEnabled || !Config.AutoUse)
+                    return;
+                __instance.recipeList = __state;
+            }
+        }
+        [HarmonyPatch(typeof(GameMenu), new Type[] { typeof(bool)})]
+        [HarmonyPatch(MethodType.Constructor)]
+        public class GameMenu_Patch
+        {
+            public static void Postfix(GameMenu __instance)
+            {
+                if (!Config.ModEnabled)
+                    return;
+                gameMenu = null;
+                resourceButton = new ClickableTextureComponent("Up", new Rectangle(__instance.xPositionOnScreen + __instance.width + 8 + Config.IconOffsetX, __instance.yPositionOnScreen + 256 + Config.IconOffsetY, 44, 44), "", SHelper.Translation.Get("resources"), Game1.mouseCursors, new Rectangle(116, 442, 22, 22), 2)
+                {
+                    myID = 42999,
+                    upNeighborID = 106,
+                    downNeighborID = 105,
+                    leftNeighborID = 11
+                };
+            }
+        }
+        [HarmonyPatch(typeof(InventoryPage), new Type[] {typeof(int),typeof(int),typeof(int),typeof(int) })]
+        [HarmonyPatch(MethodType.Constructor)]
+        public class InventoryPage_Patch
+        {
+            public static void Postfix(InventoryPage __instance)
+            {
+                if (!Config.ModEnabled || Game1.activeClickableMenu is not GameMenu)
+                    return;
+                __instance.organizeButton.downNeighborID = 42999;
+                __instance.trashCan.upNeighborID = 42999;
+            }
+        }
+        [HarmonyPatch(typeof(IClickableMenu), nameof(IClickableMenu.populateClickableComponentList))]
+        public class IClickableMenu_populateClickableComponentList_Patch
+        {
+            public static void Postfix(IClickableMenu __instance)
+            {
+                if (!Config.ModEnabled || Game1.activeClickableMenu is not GameMenu || __instance is not InventoryPage || resourceButton is null)
+                    return;
+                __instance.allClickableComponents.Add(resourceButton);
+
+            }
+        }
+        [HarmonyPatch(typeof(InventoryPage), nameof(InventoryPage.draw))]
+        public class InventoryPage_draw_Patch
+        {
+            public static void Prefix(SpriteBatch b)
+            {
+                if (!Config.ModEnabled || Game1.activeClickableMenu is not GameMenu)
+                    return;
+                resourceButton.bounds = new Rectangle(Game1.activeClickableMenu.xPositionOnScreen + Game1.activeClickableMenu.width + 8 + Config.IconOffsetX, Game1.activeClickableMenu.yPositionOnScreen + 256 + Config.IconOffsetY, 44, 44);
+                resourceButton.draw(b);
+            }
+        }
+        [HarmonyPatch(typeof(InventoryPage), nameof(InventoryPage.performHoverAction))]
+        public class InventoryPage_performHoverAction_Patch
+        {
+            public static bool Prefix(ref string ___hoverText, int x, int y)
+            {
+                if (!Config.ModEnabled || Game1.activeClickableMenu is not GameMenu)
+                    return true;
+                if(resourceButton.containsPoint(x, y))
+                {
+                    ___hoverText = resourceButton.hoverText;
+                    return false;
+                }
+                return true;
+            }
+        }
+        [HarmonyPatch(typeof(InventoryPage), nameof(InventoryPage.receiveKeyPress))]
+        public class InventoryPage_receiveKeyPress_Patch
+        {
+            public static bool Prefix(InventoryPage __instance, Keys key, ref string ___hoverText)
+            {
+                if (!Config.ModEnabled || Game1.activeClickableMenu is not GameMenu)
+                    return true;
+                if(key == (Keys)Config.ResourcesKey)
+                {
+                    ___hoverText = "";
+                    Game1.playSound("bigSelect");
+                    gameMenu = Game1.activeClickableMenu as GameMenu;
+                    Game1.activeClickableMenu = new ResourceMenu();
+                    return false;
+                }
+                return true;
+            }
+        }
+        [HarmonyPatch(typeof(InventoryPage), nameof(InventoryPage.receiveLeftClick))]
+        public class InventoryPage_receiveLeftClick_Patch
+        {
+            public static bool Prefix(InventoryPage __instance, ref string ___hoverText, int x, int y)
+            {
+                if (!Config.ModEnabled || Game1.activeClickableMenu is not GameMenu)
+                    return true;
+                if(resourceButton.containsPoint(x, y))
+                {
+                    if(Game1.player.CursorSlotItem is Object obj)
+                    {
+                        if (CanStore(obj) && Game1.objectData.ContainsKey(obj.ItemId))
+                        {
+                            Game1.playSound("Ship");
+                            ModifyResourceLevel(Game1.player, obj.QualifiedItemId, Game1.player.CursorSlotItem.Stack, false);
+                            Game1.player.CursorSlotItem = null;
+                        }
+                    }
+                    else
+                    {
+                        ___hoverText = "";
+                        Game1.playSound("bigSelect");
+                        gameMenu = Game1.activeClickableMenu as GameMenu;
+                        Game1.activeClickableMenu = new ResourceMenu();
+                        
+                    }
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public static void Leclair_Stardew_Common_InventoryHelper_CountItem_Postfix(Farmer who, Func<Item, bool> matcher, ref int __result)
+        {
+            if (!Config.ModEnabled)
+                return;
+            var resDict = GetFarmerResources(who);
+            foreach(var res in resDict)
+            {
+                Object obj = ItemRegistry.Create<Object>(res.Key, (int)res.Value);
+                if (matcher(obj))
+                {
+                    __result = (int.MaxValue - (int)res.Value < __result) ? int.MaxValue : (int)res.Value + __result;
+                    return;
+                }
+            }
+        }
+        public static void Leclair_Stardew_Common_InventoryHelper_ConsumeItem_Prefix(Func<Item, bool> matcher, IList<Item> items, int amount)
+        {
+            if (!Config.ModEnabled || items != Game1.player.Items)
+                return;
+
+            var resDict = GetFarmerResources(Game1.player);
+            foreach(var res in resDict)
+            {
+                Object obj = ItemRegistry.Create<Object>(res.Key, (int)res.Value);
+                if (matcher(obj))
+                {
+                    amount += (int)ModifyResourceLevel(Game1.player, res.Key, -amount);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ ResourceStorage/Methods.cs
@@ -0,0 +1,172 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+using StardewValley;
+using StardewValley.GameData.Objects;
+using StardewValley.Inventories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Object = StardewValley.Object;
+
+namespace ResourceStorage
+{
+    public partial class ModEntry
+    {
+        public static long ModifyResourceLevel(Farmer instance, string id, int amountToAdd, bool auto = true)
+        {
+            Dictionary<string, long> dict = GetFarmerResources(instance);
+            
+            if(!dict.TryGetValue(id, out long oldAmount))
+            {
+                if (auto && !CanAutoStore(id))
+                    return 0;
+                oldAmount = 0;
+            }
+            var newAmount = Math.Max(oldAmount + amountToAdd, 0);
+            if(newAmount != oldAmount)
+            {
+                SMonitor.Log($"Modified resource {id} from {oldAmount} to {newAmount}");
+                if (Config.ShowMessage)
+                {
+                    Object item =  ItemRegistry.Create<Object>(id, (int)(newAmount - oldAmount));
+                    try
+                    {
+                        var hm = new HUDMessage(string.Format(newAmount > oldAmount ? SHelper.Translation.Get("added-x-y") : SHelper.Translation.Get("removed-x-y"), (int)Math.Abs(newAmount - oldAmount), item.DisplayName), 1000) { whatType = newAmount > oldAmount ? 4 : 3 };
+                        Game1.addHUDMessage(hm);
+                    }
+                    catch { }
+                }
+            }
+            if (newAmount <= 0)
+                dict.Remove(id);
+            else
+                dict[id] = newAmount;
+            return newAmount - oldAmount;
+        }
+
+        public static Dictionary<string, long> GetFarmerResources(Farmer instance)
+        {
+            if (!resourceDict.TryGetValue(instance.UniqueMultiplayerID, out var dict))
+            {
+                dict = instance.modData.TryGetValue(dictKey, out var str) ? DeserializeDictionary(str) : new();
+                resourceDict[instance.UniqueMultiplayerID] = dict;
+            }
+            return dict;
+        }
+
+        public static Dictionary<string, long> DeserializeDictionary(string json)
+        {
+            return MigrateDictionary(JsonConvert.DeserializeObject<Dictionary<string, long>>(json));
+        }
+
+        /// <summary>
+        /// The resource dictionary used to (pre-SDV 1.6) use the first four items in an object's object data as the key. I now use the qualified item id. This migrates
+        /// the old keys so that old saves don't lose their resources.
+        /// </summary>
+        /// <param name="oldDictionary"></param>
+        /// <returns></returns>
+        public static Dictionary<string, long> MigrateDictionary(Dictionary<string, long> oldDictionary)
+        {
+            Dictionary<string, long> newDictionary = new();
+
+            bool first = true;
+
+            foreach(var kvp in oldDictionary)
+            {
+                if(first && ItemRegistry.IsQualifiedItemId(kvp.Key)) // If the key starts with an open parentheses, it has already been migrated
+                {
+                    SMonitor.Log($"The dictionary has already been migrated. The first key is {kvp.Key}.");
+                    return oldDictionary;
+                }
+                else
+                {
+                    first = false;
+                }
+
+                string[] oldData = kvp.Key.Split("/");
+                if(oldData.Length != 4) // The old dictionary used the first four items as keys, so it should be four long.
+                {
+                    SMonitor.Log($"The dictionary has not been migrated but the data count is not four! Key: {kvp.Key}");
+                    continue;
+                }
+
+                foreach(var objectKvp in Game1.objectData)
+                {
+                    if(objectKvp.Value.Name == oldData[0] && objectKvp.Value.Category != Object.litterCategory) // The litter check is necessary to prevent stone from turning into a gem rock because those are called stone too and they come before the material stone in the dictionary
+                    {
+                        newDictionary["(O)" + objectKvp.Key] = kvp.Value;
+                        break;
+                    }
+                }
+            }
+
+            return newDictionary;
+        }
+
+        public static long GetResourceAmount(Farmer instance, string id)
+        {
+            Dictionary<string, long> dict = GetFarmerResources(instance);
+
+            return dict.TryGetValue(id, out long amount) ? amount : 0;
+        }
+        public static bool CanStore(Object obj)
+        {
+            bool output = !(obj.Quality > 0 || obj.preserve.Value is not null || obj.orderData.Value is not null || obj.preservedParentSheetIndex.Value is not null || obj.bigCraftable.Value || obj.GetType() != typeof(Object) || obj.maximumStackSize() == 1);
+            //SMonitor.Log(output ? $"Can store {obj.DisplayName}" : $"Cannot store {obj.DisplayName}");
+            return output;
+        }
+        public static bool CanAutoStore(Object obj)
+        {
+            if (!CanStore(obj))
+                return false;
+            
+            return CanAutoStore(obj.ItemId);
+        }
+
+        public static bool CanAutoStore(string id)
+        {
+            if (Game1.objectData.TryGetValue(DequalifyItemId(id), out ObjectData data))
+            {
+                foreach (var str in Config.AutoStore.Split(','))
+                {
+                    if(str.Trim() == data.Name && data.Category != Object.litterCategory)
+                    {
+                        //SMonitor.Log($"Can astore {data.DisplayName}");
+                        return true;
+                    }
+                }
+                //SMonitor.Log($"Cannot astore {data.DisplayName}");
+            }
+            return false;
+        }
+
+        public static string DequalifyItemId(string id)
+        {
+            return ItemRegistry.GetMetadata(id).LocalItemId;
+        }
+
+        private static int AddIngredientAmount(int ingredient_count, KeyValuePair<string, int> pair)
+        {
+            if (!Config.ModEnabled || !Config.AutoUse || !Game1.objectData.ContainsKey(pair.Key))
+                return ingredient_count;
+
+            return (int)(ingredient_count + GetResourceAmount(Game1.player, ItemRegistry.QualifyItemId(pair.Key)));
+        }
+
+        public static bool TryGetInventoryOwner(Inventory inventory, out Farmer farmer)
+        {
+            foreach(Farmer f in Game1.getAllFarmers())
+            {
+                if(f.Items == inventory)
+                {
+                    farmer = f;
+                    return true;
+                }
+            }
+
+            farmer = null;
+            return false;
+        }
+    }
+}

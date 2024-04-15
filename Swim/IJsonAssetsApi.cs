f3d27e312f2b1a1f1b020e02dc03b523b8eb6bde
C. Cooper <c.cooper@ku.edu>
2024-04-14 21:37:44 -0500

    Add Swim files

---
+++ Swim/IJsonAssetsApi.cs
@@ -0,0 +1,9 @@
+﻿namespace Swim
+{
+    public interface IJsonAssetsApi
+    {
+        string GetClothingId(string name);
+        string GetHatId(string name);
+        void LoadAssets(string path);
+    }
+}

c1e7c7ec4c6cb3f00451cfd2d125ae1385efe7ce
C. Cooper <c.cooper@ku.edu>
2024-04-24 20:20:11 -0500

    Make auto swim false by default
    It being too sensitive is one of the most comment complaints I see on the old mod page

--- Swim/ModConfig.cs
+++ Swim/ModConfig.cs
@@ -54,7 +54,7 @@ namespace Swim
             ManualJumpButton = SButton.MouseRight;
 
             EnableMod = true;
-            ReadyToSwim = true;
+            ReadyToSwim = false;
             SwimIndoors = false;
             ShowOxygenBar = true;
             SwimSuitAlways = false;

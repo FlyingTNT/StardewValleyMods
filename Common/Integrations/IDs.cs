74dd004e84a38fd98b024ef50e26131df7f291ba
C. Cooper <c.cooper@ku.edu>
2024-10-13 13:22:10 -0500

    Create IDs.cs
    Central place to hold mod ids

---
+++ Common/Integrations/IDs.cs
@@ -0,0 +1,7 @@
+namespace Common.Integrations;
+
+public static class IDs
+{
+    public const string ContentPatcher = "Pathoschild.ContentPatcher";
+    public const string GMCM = "spacechase0.GenericModConfigMenu";
+}

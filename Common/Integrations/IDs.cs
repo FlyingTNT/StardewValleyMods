6652761ab3e3e68143eff0eb420304246bb3da2f
C. Cooper <c.cooper@ku.edu>
2024-11-06 09:44:57 -0600

    Update IDs.cs

--- Common/Integrations/IDs.cs
+++ Common/Integrations/IDs.cs
@@ -4,4 +4,5 @@ public static class IDs
 {
     public const string ContentPatcher = "Pathoschild.ContentPatcher";
     public const string GMCM = "spacechase0.GenericModConfigMenu";
+    public const string LongerSeasons = "FlyingTNT.LongerSeasons";
 }

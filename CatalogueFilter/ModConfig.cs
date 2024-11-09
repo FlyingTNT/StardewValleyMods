83f527ba5423172c9b5cb952d15ccc45a95f6141
C. Cooper <c.cooper@ku.edu>
2024-11-09 12:26:56 -0600

    Update Catalogue Filter
    Compatibility with SDV 1.6.9
    Adds splitscreen support
    Adds config options to move the filter and select it automatically
    Adds a keybind to select the filter
    Adds a Spanish transltion
    Removes use of Harmony.PatchAll

--- CatalogueFilter/ModConfig.cs
+++ CatalogueFilter/ModConfig.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
 using System.Collections.Generic;
 
 namespace CatalogueFilter
@@ -9,5 +10,9 @@ namespace CatalogueFilter
         public bool ModEnabled { get; set; } = true;
         public bool ShowLabel { get; set; } = true;
         public Color LabelColor { get; set; } = Color.White; // Color for the text that says "Filter" in the shop menu
+        public bool AutoSelectFilter { get; set; } = false;
+        public int FilterOffsetX { get; set; } = 0;
+        public int FilterOffsetY { get; set; } = 0;
+        public KeybindList SelectFilterKey { get; set; } = KeybindList.Parse("Tab");
     }
 }

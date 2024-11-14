3fdbf98a625d375b3589c0eaf6c463b64ebd5141
C. Cooper <c.cooper@ku.edu>
2024-11-13 18:31:40 -0600

    Move the QuickSave API to Common/Integrations

--- Common/Integrations/IDs.cs
+++ Common/Integrations/IDs.cs
@@ -4,5 +4,7 @@ public static class IDs
 {
     public const string ContentPatcher = "Pathoschild.ContentPatcher";
     public const string GMCM = "spacechase0.GenericModConfigMenu";
+    public const string QuickSave = "DLX.QuickSave";
+
     public const string LongerSeasons = "FlyingTNT.LongerSeasons";
 }

8c347559f511df2ba53aec657b7f57bb3b89a0bc
C. Cooper <c.cooper@ku.edu>
2025-05-29 16:08:56 -0500

    Update IDs.cs

--- Common/Integrations/IDs.cs
+++ Common/Integrations/IDs.cs
@@ -5,6 +5,8 @@ public static class IDs
     public const string ContentPatcher = "Pathoschild.ContentPatcher";
     public const string GMCM = "spacechase0.GenericModConfigMenu";
     public const string QuickSave = "DLX.QuickSave";
+    public const string Survivalistic = "Ophaneom.Survivalistic"; // New version is an unofficial update and uses the same id
+    public const string GemIsles = "aedenthorn.GemIsles";
     public const string BetterGameMenu = "leclair.bettergamemenu";
 
     public const string LongerSeasons = "FlyingTNT.LongerSeasons";
